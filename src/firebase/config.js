e983f1f99d963082150095d4972ca879d3b02624
javimatz <jvdelso@gmail.com>
2020-08-11 12:18:44 +0200

    add: starter files

---
+++ src/firebase/config.js
@@ -0,0 +1,22 @@
+import * as firebase from 'firebase/app';
+import 'firebase/storage';
+import 'firebase/firestore';
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+	apiKey: "AIzaSyDm4iAqIuDzl9PNSjt0rTzBOZ5mVRZ-WaM",
+	authDomain: "react-photo-gallery-fcabe.firebaseapp.com",
+	databaseURL: "https://react-photo-gallery-fcabe.firebaseio.com",
+	projectId: "react-photo-gallery-fcabe",
+	storageBucket: "react-photo-gallery-fcabe.appspot.com",
+	messagingSenderId: "786960654422",
+	appId: "1:786960654422:web:cd2e075dc5d0c1f16a5bf7"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const projectStorage = firebase.storage();
+const projectFirestore = firebase.firestore();
+
+export { projectStorage, projectFirestore};
